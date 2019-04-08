646b30cdf21f6ad0d64ff79b66636b6058a48423
unknown <agusfernandez1988@gmail.com>
2019-04-07 22:01:27 -0300

    real time clock

--- 02 - JS and CSS Clock/scripts.js
+++ 02 - JS and CSS Clock/scripts.js
@@ -2,36 +2,35 @@ const secondHand = document.querySelector('.second-hand');
 const minsHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 
-function setDate(){
+function setDate() {
 
-let now = new Date();
-
-
-// // Set seconds
-
-let seconds = now.getSeconds();
-let secondsDegree = seconds * 6;
-secondHand.style.transform = `rotate(${(secondsDegree)+90}deg)`;
+  let now = new Date();
 
 
+  // // Set seconds
 
-// Set minutes
-
-let mins = now.getMinutes();
-let minsDegrees = mins * 6
-minsHand.style.transform = `rotate(${(minsDegrees)+90}deg)`;
+  let seconds = now.getSeconds();
+  let secondsDegree = seconds * 6;
+  secondHand.style.transform = `rotate(${(secondsDegree)+90}deg)`;
 
 
-// Set hours
 
-let hour = now.getHours();   
+  // Set minutes
 
-if (hour > 12) {
+  let mins = now.getMinutes();
+  let minsDegrees = mins * 6
+  minsHand.style.transform = `rotate(${(minsDegrees)+90}deg)`;
+
+
+  // Set hours
+
+  let hour = now.getHours();
+
+  if (hour > 12) {
     hour = hour - 12;
-}
-let hourDegress = hour * 30;
-hourHand.style.transform = `rotate(${(hourDegress)+90}deg)`;
+  }
+  let hourDegress = hour * 30;
+  hourHand.style.transform = `rotate(${(hourDegress)+90}deg)`;
 };
 
 setInterval(setDate, 1000);
-  
